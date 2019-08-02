a21211606d72eb170e79d02ff21a1ee985598a5f
skyyy6 <35333225+skyyy6@users.noreply.github.com>
2019-08-02 16:45:51 +0900

    190802
    + 흥미검사

--- careerbot/answertest.py
+++ careerbot/answertest.py
@@ -127,6 +127,67 @@ def call_worknet():
 
     return SendMessage(dataSend)
 
+# 흥미검사 스킬
+@app.route('/check_interest', methods=['post'])
+def check_interest():
+    req = request.get_json()
+    content = req['action']
+    content = content['detailParams']
+
+    interest = []
+    result = ""
+
+    for key in content.keys():
+        if content[key]['value'] not in interest:
+            interest.append(content[key]['value'])
+
+    reply = []
+    for i in interest:
+        result = result + i + " "
+
+        re = {
+            "messageText": i,
+            "action": "message",
+            "label": i
+        }
+        reply.append(re)
+
+    comment = [makeSimpleText("흥미 검사 결과 " + result + "이 나왔습니다. 각 흥미에 대해 알고싶다면 아래 버튼을 눌러주세요")]
+
+    return SendReply(comment, reply)
+
+# 흥미검사 답변 스킬
+@app.route('/interest_result', methods=['post'])
+def interest_result():
+    req = request.get_json()
+    content = req['action']
+    content = content['detailParams']
+
+    comment = {
+        "현실형" : "특징\n분명하고 질서정연하고 체계적인 것을 좋아하고, 연장/기계를 조작하는 활동내지는 기술에 흥미로워 함"
+                "\n\n성격\n현실적이고 신중한 성격"
+                "\n\n선호하는 직업\n소방관,군인,경찰",
+        "탐구형" : "특징\n관찰적,상직적,체계적이며 물리적,생물학적,문화적 현상의 창조적인 탐구를 수반하는 활동을 흥미로워 함"
+                "\n\n성격\n분석적이고 지적인 성격"
+                "\n\n선호하는 직업\n언어학자, 심리학자, 물리학자",
+        "예술형" : "특징\n예술/창조적 표현, 변화와 다양성을 선호하고 틀에 박힌 것 보다는 자유롭고, 상징적인 활동에 흥미로워 함"
+                "\n\n성격\n경험에 대해 개방적인 성격"
+                "\n\n선호하는 직업\n음악가, 화가, 디자이너",
+        "사회형" : "특징\n타인의 문제를 듣고, 이해하고, 잘 도와주며 치료해주고 봉사하는 활동에 흥미로워 함"
+                "\n\n성격\n배려심과 친화력이 있는 성격"
+                "\n\n선호하는 직업\n교육자,상담가,간호사",
+        "관습형" : "특징\n정해진 원칙과 계획에 따라 자료를 정리/조작하는 일을 좋아하고 체계적인 작업환경에서 사무적, 계산적 능력을 발휘하는 활동에 흥미로워 함"
+                "\n\n성격\n조용하고 차분한 성격"
+                "\n\n선호하는 직업\n금융사무원,노무사,회계사",
+        "진취형" : "특징\n조직의 목적과 이익을 얻기 위해 타인을 지도/계획/통제/관리 일과 그 결과로 얻어지는 명예/권위에 흥미로워 함"
+                "\n\n성격\n진취적이고 외향적인 성격"
+                "\n\n선호하는 직업\n영업/판매업무, CEO, 마케터"
+    }
+
+    result = content['흥미검사']['value']
+
+    return SendMessage([makeSimpleText(comment[result])])
+
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=5000, debug=True)
