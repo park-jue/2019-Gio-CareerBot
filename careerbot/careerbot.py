567e6f6f3a0b02dc20c050c951cd40b591889eed
skyyy6 <35333225+skyyy6@users.noreply.github.com>
2019-08-19 18:00:25 +0900

    190819
    성격검사 추가

--- careerbot/careerbot.py
+++ careerbot/careerbot.py
@@ -9,7 +9,7 @@ import urllib.request
 app = Flask(__name__)
 firebase = firebase.FirebaseApplication("https://gio-careerbot-c1797.firebaseio.com/", None)
 
-# 일반 말풍선 출력 / message_list : json list형식
+# 일반 말풍선 출력 템플릿 / message_list : json list형식
 def SendMessage(message_list):
     temp = {
         "version": "2.0",
@@ -19,7 +19,7 @@ def SendMessage(message_list):
     }
     return jsonify(temp)
 
-# 말풍선 + 바로가기 버튼 출력 / message_list, reply_list : json list형식
+# 말풍선 + 바로가기 버튼 출력 템플릿 / message_list, reply_list : json list형식
 def SendReply(message_list, reply_list):
     temp = {
         "version": "2.0",
@@ -64,7 +64,7 @@ def fallback():
         context_list.append(c['name'])
 
     if len(context) != 0:
-        if 'check_career' in context_list: # 진로고민 분기 context
+        if 'check_career' in context_list: # 진로고민 분기 context fallback
             return SendMessage([makeSimpleText('네/아니오로 다시 한번 대답해주세요')])
 
         elif 'input_name' in context_list: # 인적사항_입력 context
@@ -76,6 +76,9 @@ def fallback():
             firebase.patch('/User/' + Uid, {'experience': answer})
             firebase.patch('/User/' + Uid, {'isNull' : 0}) # 인적사항이 다 입력되면 isNull == 0
             return SendMessage([makeSimpleText("인적사항 입력이 끝났습니다!\n상담을 진행하시려면 아래 상담 시작하기 버튼을 눌러주세요.")])
+
+        elif 'mbti_check' in context_list : # mbti 검사 context fallback
+            return SendMessage([makeSimpleText('성격유형을 확인한 후 다시 한번 입력해주세요.')])
 
     i = random.randint(0, 2)
     comment = ['무엇을 원하는지 잘 모르겠어요', '이해하기 어려워요', '제가 할 수 있는 일이 아니에요']
@@ -121,20 +124,33 @@ def get_information():
 
     return SendMessage([makeSimpleText("")])
 
-# 진로고민 분기 스킬 / 발화조건 => 대답 Entity
-@app.route('/career_branch', methods = ['post'])
-def career_branch():
+# 분기 스킬 / 발화조건 => 대답 Entity
+@app.route('/branch', methods = ['post'])
+def branch():
     req = request.get_json()
     answer = req['action']['detailParams']['답변']['value'] # 긍정 or 부정
+    context = req['contexts']
 
-    if answer == '긍정':
-        return SendMessage([makeSimpleText('와 준비된 분이시군요?\n\n 어떤 일을 생각하시고 계신가요?')])
-    elif answer == '부정':
-        result = firebase.get('/UI/career_branch/disagree', None)
+    context_list = []
+    for c in context:
+        context_list.append(c['name'])
 
-        return jsonify(result)
-    else:
-        return SendMessage([makeSimpleText('예/아니오로 다시 대답해주세요.')])
+    if 'check_career' in context_list: # 진로_분기
+        if answer == '긍정':
+            return SendMessage([makeSimpleText('와 준비된 분이시군요?\n\n 어떤 일을 생각하시고 계신가요?')])
+        else:
+            result = firebase.get('/UI/career_branch/disagree', None)
+            return jsonify(result)
+
+    else: #직업선택_분기
+        if answer == '긍정':
+            result = firebase.get('/UI/result_branch/agree', None)
+            return jsonify(result)
+        else:
+            result = makeSimpleText(firebase.get('/UI/result_branch/disagree', None))
+            return SendMessage([result])
+
+    return SendMessage([makeSimpleText('')])
 
 
 # 희망진로 스킬
@@ -161,10 +177,10 @@ def check_interest():
     content = req['action']['detailParams']['number']['value']
     index = req['action']['detailParams']['index']['value']
 
-    firebase.patch("/User/" + Uid + "/check_interest", { index: int(content) })
+    firebase.patch("/User/" + Uid + "/interest_inventory/check_interest", { index: int(content) })
 
-    if(context[0]['name'] == 'check_interest_6'):
-        result = firebase.get('/User/' + Uid + "/check_interest", None)
+    if context[0]['name'] == 'check_interest_6':
+        result = firebase.get('/User/' + Uid + "/interest_inventory/check_interest", None)
         result_sort = sorted(result.items(), key=operator.itemgetter(1), reverse=True)
 
         interest_list = []
@@ -177,7 +193,7 @@ def check_interest():
 
             interest_list.append(r[0])
 
-        firebase.patch('/User/' + Uid, {"interest" : interest_list})
+        firebase.patch('/User/' + Uid + '/interest_inventory', {"interest" : interest_list})
 
         reply = []
         interest = ""
@@ -225,7 +241,7 @@ def interest_back():
     req = request.get_json()
     Uid = req['userRequest']['user']['id']
 
-    interest_list = firebase.get('/User/' + Uid + "/interest", None)
+    interest_list = firebase.get('/User/' + Uid + "/interest_inventory/interest", None)
     reply = []
     interest = ""
     for i in interest_list:
@@ -238,9 +254,10 @@ def interest_back():
         }
         reply.append(re)
 
-    comment = [makeSimpleText("흥미 검사 결과 " + interest + "이 나왔습니다. 각 흥미유형을 보고 마음에 드는 직업 3가지를 입력해주세요.")]
+    comment_1 = makeSimpleText("흥미 검사 결과 " + interest + "이 나왔습니다. 각 흥미에 대해 알고싶다면 아래 버튼을 눌러주세요.")
+    comment_2 = makeSimpleText("유형별 대표직업을 확인했습니다. 그중 마음에 드는 직업을 선택하셨나요??")
 
-    return SendReply(comment, reply)
+    return SendReply([comment_1, comment_2], reply)
 
 # 흥미검사 _ 직업 선택
 @app.route('/select_job', methods = ['post'])
@@ -295,13 +312,52 @@ def calculate_job():
                 max = result[i]['score']
                 rec_job = result[i]['name']
 
-        return SendMessage([makeSimpleText(user + "님께는 '" + rec_job + "'직업이 적합한 것 같아요.\n이 직업에 대한 정보를 찾아보시겠어요?")])
+        comment = []
+        comment.append(makeSimpleText("3가지 직업중 '" + rec_job + "'직업을 가장 선호하시는군요."))
+        comment.append(makeSimpleText("잘 결정하셨네요^^ " + user + "님은 잘하실 수 있을 겁니다. "))
+        comment.append(makeSimpleText("이 직업의 간단한 정보를 안내해 드릴게요"))
+
+        return SendMessage(comment)
 
     comment = []
     comment.append(makeSimpleText("다음으로 " + next_job + "에 대한 점수를 매겨주세요."))
     comment.append(makeSimpleText(firebase.get('/UI/interest_result/calculate_job', None)))
 
     return SendMessage(comment)
+
+# 성격검사 스킬
+@app.route('/check_MBTI', methods = ['post'])
+def check_MBTI():
+    req = request.get_json()
+    Uid = req['userRequest']['user']['id']
+    context = req['contexts']
+    user = firebase.get('/User/' + Uid + "/name", None)
+
+    context_list = []
+    for c in context:
+        context_list.append(c['name'])
+
+    mbti = firebase.get('/User/' + Uid + '/MBTI', None)
+
+    input_mbti = req['action']['detailParams']['mbti']['value']
+
+    if 'mbti_1' not in context_list:
+        input_mbti = mbti + input_mbti
+
+    firebase.patch('/User/' + Uid, {'MBTI': input_mbti})
+
+    if 'mbti_4' in context_list:
+        mbti = firebase.get('/User/' + Uid + '/MBTI', None)
+
+        comment = makeSimpleText(user + '님의 ' + firebase.get('/UI/mbti/' + mbti + '/description', None))
+        career = makeSimpleText('추천직업으로는 ' + firebase.get('/UI/mbti/' + mbti + '/career', None) + '가 있어요')
+        qes = makeSimpleText("성격유형별 대표직업을 확인했습니다. 그중 마음에 드는 직업을 선택하셨나요??")
+
+        return SendMessage([comment, career, qes])
+
+    return SendMessage([makeSimpleText('')])
+
+
 
 
 if __name__ == "__main__":
